d6472eedcda605720fde3d623137aeacb640530a
Nikita Lukonenko <nikitalukonenko@MacBook-Pro-Nikita-3.local>
2024-05-29 02:08:25 +0300

    fix: some little fixes

--- untitled/src/main/kotlin/Nodes/AbstractNode.kt
+++ untitled/src/main/kotlin/Nodes/AbstractNode.kt
@@ -16,23 +16,4 @@ abstract class AbstractNode<K : Comparable<K>, V, N : AbstractNode<K, V, N>>(
 
     @Suppress("UNCHECKED_CAST")
     open suspend fun min(): N = this.left?.min() ?: (this as N)
-
-
-//    fun buildString(sb: StringBuilder, padding: String = "", pointer: String = "") {
-//        sb.append(padding)
-//        sb.append(pointer)
-//        sb.append("($key,$value)")
-//        sb.append("\n")
-//
-//        val paddingBuilder = StringBuilder(padding)
-//        paddingBuilder.append("│  ")
-//
-//        val paddingForBoth = paddingBuilder.toString()
-//        val pointerForRight = "└──"
-//        val pointerForLeft = if (right != null) "├──" else "└──"
-//
-//        left?.buildString(sb, paddingForBoth, pointerForLeft)
-//        right?.buildString(sb, paddingForBoth, pointerForRight)
-//
-//    }
 }
--- untitled/src/main/kotlin/Nodes/FineNode.kt
+++ untitled/src/main/kotlin/Nodes/FineNode.kt
@@ -1,4 +1,3 @@
-//package org.example.Nodes
 
 import kotlinx.coroutines.sync.Mutex
 
